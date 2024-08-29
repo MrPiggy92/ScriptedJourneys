e6303d32ed86c7ccba26241e5601392f2c72c5ac
Mr-Piggy <samstuff@samstuff.top>
2024-08-29 10:38:04 +0100

    Prepared for flathub

--- flatpak/flatpak/src/spell.py
+++
@@ -1,6 +0,0 @@
-class Spell:
-    def __init__(self, num, name, description, effect):
-        self.num = num
-        self.name = name
-        self.description = description
-        self.effect = effect
